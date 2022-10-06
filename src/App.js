9db6f4db942b5edb59786bcad246c64eb955e748
omarusmani <omarusmani3@hotmail.com>
2022-10-06 08:08:07 -0500

    finished deltion of posts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {React, useEffect, useState } from 'react';
+import {React, useEffect, useInsertionEffect, useState } from 'react';
 import Axios from 'axios';
 import { Button } from '@mui/material';
 import {Link} from "@mui/material"
@@ -19,29 +19,29 @@ function App() {
 
   const [users, setUsers] = useState([{
     id: 0,
-    first: "",
-    last: "",
+    first_name: "Dummy",
+    last_name: "",
     email:"",
     username: ""
 },{id: 1,
-  first: "",
-  last: "",
+  first_name: "",
+  last_name: "",
   email:"",
   username: ""}]);//had to create in order to access props of json
   const [posts, setPosts] = useState([{
     id: 0,
-    username: 1,
-    post: "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
-    url: "http://dummyimage.com/439x354.png/dddddd/000000",
-    Title: "Spencer's Mountain",
-    Days: "2022-08-25T00:00:00.000Z"
+    username: 800000,
+    post: "Dummy Text, You Logged I without a username",
+    url: "",
+    Title: "Dummy Title",
+    Days: ""
 },{
   id: 1,
-  username: 1,
-  post: "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
-  url: "http://dummyimage.com/439x354.png/dddddd/000000",
-  Title: "Spencer's Mountain",
-  Days: "2022-08-25T00:00:00.000Z"
+  username: 800000,
+  post: "Dummy Text, You Logged I without a username",
+  url: "",
+  Title: "Dummy Title",
+  Days: ""
 }]);
   const [password, setPassword] = useState("");
   const [first, setFirst] = useState("");
@@ -50,6 +50,7 @@ function App() {
   const [email, setEmail] = useState("");
   const [post,setPost]=useState("");
   const [title,setTitle]=useState("");
+
 
 
 
@@ -62,26 +63,36 @@ function App() {
 
   // GET with Axios
 
-const fetchPost = async (u) => {
+const fetchPost = async () => {
       try {
-        let response = await client.get(`/post/${u}`);
+        let response = await client.get(`/post/${users.id}`);
         setPosts(response.data);
       } catch (error) {
         console.log(error);
      }
      };
 
+
+
+
 useEffect(() => {
-const fetchUsers = async () => {
+
+  setTimeout(() => {
+        
+  }, 180);
+      const fetchUsers = async () => {
      try {
        let response = await client.get(`/user/${username}`);
-       setUsers(response.data);
-     } catch (error) {
+       setUsers(response.data[0]);
+       console.log(users)
+      } catch (error) {
        console.log(error);
     }
   };
-  fetchUsers();
+  fetchUsers()
 }, [username]);
+
+
 
 
  
@@ -90,20 +101,29 @@ const fetchUsers = async () => {
  const sendResults=()=>{
 
  }
+ const deletePost=(e,x)=>{
+  e.preventDefault()
+  client.delete(`/post/${x}`);
+  setPosts(
+    posts.filter((y) => {
+       return y.id !== x;
+    }))
+  fetchPost()
+}
 
  
  const handleLogin=(e)=>{
    e.preventDefault();
-   fetchPost(users[0].id)
   setisHidden(!isHidden)
+  fetchPost(users.id)
 
  } 
- const handleSignup=(e)=>{
-  e.preventDefault()
-  setisSigned(!isSigned)
+//  const handleSignup=(e)=>{
+//   e.preventDefault()
+//   setisSigned(!isSigned)
 
- }
-// console.log(users[0].id,posts,username)
+//  }
+console.log(users,posts,username)
 // console.log(users,posts,last,first,email,post,title,username,password)
 const clearState=()=>{
   setUsername("")
@@ -113,19 +133,33 @@ const clearState=()=>{
   setEmail("")
   setPost("")
   setTitle("")
-  setPosts([{id:0}])
+  setPosts([{
+    id: 0,
+    username: 800000,
+    post: "",
+    url: "",
+    Title: "",
+    Days: ""
+},{
+  id: 1,
+  username: 800000,
+  post: "",
+  url: "",
+  Title: "",
+  Days: ""
+}])
   setUsers([{
     id: 0,
-    first: "",
-    last: "",
+    first_name: "Dummy",
+    last_name: "",
     email:"",
     username: ""
 },{id: 1,
-  first: "",
-  last: "",
+  first_name: "Dummy",
+  last_name: "",
   email:"",
   username: ""}])
-// console.log(users,posts,last,first,email,post,title,username,password)
+console.log(users,posts,last,first,email,post,title,username,password)
 
 }
   return (
@@ -136,7 +170,7 @@ const clearState=()=>{
             <Button className="greet" variant="text">Login</Button>
           </Link>
           <Link href="#" underline="none" to="">
-            <Button onClick={(e) => {handleSignup(e);handleLogin(e);}} className="greet" variant="text">Signup</Button>
+            {/* <Button onClick={(e) => {handleSignup(e);handleLogin(e);}} className="greet" variant="text">Signup</Button> */}
     <Box
       component="form"
       sx={{
@@ -146,16 +180,16 @@ const clearState=()=>{
       autoComplete="off"
     >
       <TextField onChange={(e)=> setUsername(e.target.value)}  id="User Name" label="username" variant="filled" />
-      <TextField onChange={(e) => setPassword(e.target.value)} id="Password" label="password" variant="filled" /> 
+      {/* <TextField onChange={(e) => setPassword(e.target.value)} id="Password" label="password" variant="filled" />  */}
     </Box>
           </Link>
         </div></span>}
-          {isHidden&&<span><h1 className="greet">Welcome to Your Diary</h1><div className="center">
+          {isHidden&&<span><h1 className="greet">Welcome to Your Diary, {users.first_name}</h1><div className="center">
           <Link underline="none" to="">
-            <Button onClick={(e)=>{handleLogin(e);clearState(e);}}  className="greet" variant="text">Logout</Button>
+            <Button onClick={(e)=>{handleLogin(e);clearState();}}  className="greet" variant="text">Logout</Button>
           </Link>
           <Link underline="none" to="">
-            <Button   className="greet" variant="text">Delete Account</Button>
+            {/* <Button   className="greet" variant="text">Delete Account</Button> */}
           </Link>
           <br/>
           <TextField onChange={(e)=>setTitle(e.target.value)} fullWidth label="Title" id="fullWidth" />
@@ -174,7 +208,11 @@ const clearState=()=>{
           <br/> 
           {/* users need to be mapped by their posts */}
           <ul style={{ listStyleType: "none" }}>
-              {(posts).map((post)=> {return (<li key={post.id}><h2>{post.Title}</h2></li>)})}
+              {(posts).map((post)=> {return (<li key={post.id}>
+                <h2>{post.Title}</h2>
+                <h4>{post.post}</h4>
+                <Button onClick={(e)=>{deletePost(e,post.id)}}  className="greet" variant="text">Delete Entry</Button>
+                </li>)})}
           </ul>
 
         </div></span>}
@@ -189,9 +227,9 @@ const clearState=()=>{
       noValidate
       autoComplete="off"
       >
-            <TextField onChange={(e) => setFirst(e.target.value)} id="First_Name" label="First Name" variant="filled" />
+            {/* <TextField onChange={(e) => setFirst(e.target.value)} id="First_Name" label="First Name" variant="filled" />
             <TextField onChange={(e) => setLast(e.target.value)} id="Last_Name" label="Last Name" variant="filled" /> 
-            <TextField onChange={(e) => setEmail(e.target.value)}id="email" label="email" variant="filled" /> 
+            <TextField onChange={(e) => setEmail(e.target.value)}id="email" label="email" variant="filled" />  */}
           </Box>
           </Link>
         </span>}
