c292ecf7b107c38a2e518b3eb6415ae5662c24e8
omarusmani <omarusmani3@hotmail.com>
2022-10-08 14:16:27 -0500

    loginand sign up locks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {React, useEffect, useInsertionEffect, useState } from 'react';
+import {React, useEffect, useState } from 'react';
 import Axios from 'axios';
 import { Button } from '@mui/material';
 import {Link} from "@mui/material"
@@ -13,7 +13,6 @@ import TextareaAutosize from '@mui/material/TextareaAutosize';
 function App() {
 
   
-  const [user, setUser] = useState([]);
   const [isHidden,setisHidden]=useState(false)
   const [isSigned,setisSigned]=useState(false)
 
@@ -50,6 +49,8 @@ function App() {
   const [email, setEmail] = useState("");
   const [post,setPost]=useState("");
   const [title,setTitle]=useState("");
+  const [url,setUrl]=useState("")
+  const  [auth,setAuth]=useState(false)
 
 
 
@@ -63,44 +64,95 @@ function App() {
 
   // GET with Axios
 
-const fetchPost = async () => {
+
+  const login = async () => {
       try {
-        let response = await client.get(`/post/${users.id}`);
+        let response = await client.get(`/login/?username=${username}&password=${password}`);
+        let x= response
+        console.log(x)
+        if(x.status===401){
+          setAuth(false)
+          }
+        else {
+          setUsers(x.data)
+        setAuth(true)
+      } // fetchPost()
+             //let user be loggedin
+        
+      } catch (error) {
+        console.log(error);
+     }
+     };
+     useEffect(() => {
+     const fetchPost = async () => {
+      try {
+        let response = await client.get(`/login/post/?username=${users[0].id}`);
+        // let x = response;
+        // console.log(x)
         setPosts(response.data);
       } catch (error) {
         console.log(error);
      }
      };
+       fetchPost()
+     },);
 
 
+// useEffect(() => {
 
-
-useEffect(() => {
-
-  setTimeout(() => {
+//   setTimeout(() => {
         
-  }, 180);
-      const fetchUsers = async () => {
-     try {
-       let response = await client.get(`/user/${username}`);
-       setUsers(response.data[0]);
-       console.log(users)
-      } catch (error) {
-       console.log(error);
-    }
-  };
-  fetchUsers()
-}, [username]);
+//   }, 180);
+//       const fetchUsers = async () => {
+//      try {
+//        let response = await client.get(`/login/`);
+//       //  setUsers(response.data[0]);
+//       //  console.log(users)
+//       } catch (error) {
+//        console.log(error);
+//     }
+//   };
+//   fetchUsers()
+// },);
 
 
+const sendUser=(e)=>{
+      client
+         .post(`/login/`, {
+            first_name:first,
+            last_name:last,
+            email:email,
+            username:username,
+            password:password
+         })
+         .then((response) => {
+        })
+      setFirst('');
+      setLast('');
+      setEmail('')
+      handleLogin(e)
+ }
 
 
  
 
 
  const sendResults=()=>{
-
+      client
+         .post(`/login/post`, {
+            title: title,
+            post: post,
+            username: users.id,
+            url:url,
+         })
+         .then((response) => {
+            // fetchPost();
+        })
+      setTitle('');
+      setPost('');
+      setUrl('')
  }
+
  const deletePost=(e,x)=>{
   e.preventDefault()
   client.delete(`/post/${x}`);
@@ -108,23 +160,38 @@ useEffect(() => {
     posts.filter((y) => {
        return y.id !== x;
     }))
-  fetchPost()
+  // fetchPost()
 }
 
  
  const handleLogin=(e)=>{
    e.preventDefault();
-  setisHidden(!isHidden)
-  fetchPost(users.id)
-
+   login()
+  if(auth===true){
+    setisHidden(!isHidden)
+  }else{}
  } 
-//  const handleSignup=(e)=>{
-//   e.preventDefault()
-//   setisSigned(!isSigned)
-
-//  }
-console.log(users,posts,username)
-// console.log(users,posts,last,first,email,post,title,username,password)
+ const handleOut=(e)=>{
+  e.preventDefault();
+ setisHidden(!isHidden)
+ if(isSigned===true){
+  setisSigned(!isSigned)
+ }
+} 
+ const handleSignup =(e)=>{
+  e.preventDefault()
+  let u= username.length
+  let p= password.length
+  let l= last.length
+  let f= first.length
+  let m= email.length
+  if(u>0&&p>0&&l>0&&f>0&&m>0){
+  sendUser()
+  setisSigned(!isSigned)
+  }
+ }
+console.log(users[0])
+console.log(users[0].id,posts,last,first,email,post,title,username,password)
 const clearState=()=>{
   setUsername("")
   setPassword("")
@@ -170,7 +237,7 @@ console.log(users,posts,last,first,email,post,title,username,password)
             <Button className="greet" variant="text">Login</Button>
           </Link>
           <Link href="#" underline="none" to="">
-            {/* <Button onClick={(e) => {handleSignup(e);handleLogin(e);}} className="greet" variant="text">Signup</Button> */}
+            <Button onClick={(e) => {handleSignup(e);}} className="greet" variant="text">Signup</Button>
     <Box
       component="form"
       sx={{
@@ -180,16 +247,16 @@ console.log(users,posts,last,first,email,post,title,username,password)
       autoComplete="off"
     >
       <TextField onChange={(e)=> setUsername(e.target.value)}  id="User Name" label="username" variant="filled" />
-      {/* <TextField onChange={(e) => setPassword(e.target.value)} id="Password" label="password" variant="filled" />  */}
+      <TextField onChange={(e) => setPassword(e.target.value)} id="Password" label="password" variant="filled" /> 
     </Box>
           </Link>
         </div></span>}
           {isHidden&&<span><h1 className="greet">Welcome to Your Diary, {users.first_name}</h1><div className="center">
           <Link underline="none" to="">
-            <Button onClick={(e)=>{handleLogin(e);clearState();}}  className="greet" variant="text">Logout</Button>
+            <Button onClick={(e)=>{handleOut(e);clearState();}}  className="greet" variant="text">Logout</Button>
           </Link>
           <Link underline="none" to="">
-            {/* <Button   className="greet" variant="text">Delete Account</Button> */}
+            <Button   className="greet" variant="text">Delete Account</Button>
           </Link>
           <br/>
           <TextField onChange={(e)=>setTitle(e.target.value)} fullWidth label="Title" id="fullWidth" />
@@ -203,13 +270,14 @@ console.log(users,posts,last,first,email,post,title,username,password)
           id="Entry"
             />
           <Link underline="none" to="">
-            <Button onClick={(e)=>sendResults()}  className="greet" variant="text">Upload Entry</Button>
+            <Button onClick={(e)=>sendResults(e)}  className="greet" variant="text">Upload Entry</Button>
           </Link>
           <br/> 
           {/* users need to be mapped by their posts */}
           <ul style={{ listStyleType: "none" }}>
               {(posts).map((post)=> {return (<li key={post.id}>
                 <h2>{post.Title}</h2>
+                <img alt="" href={post.url}></img>
                 <h4>{post.post}</h4>
                 <Button onClick={(e)=>{deletePost(e,post.id)}}  className="greet" variant="text">Delete Entry</Button>
                 </li>)})}
@@ -227,9 +295,9 @@ console.log(users,posts,last,first,email,post,title,username,password)
       noValidate
       autoComplete="off"
       >
-            {/* <TextField onChange={(e) => setFirst(e.target.value)} id="First_Name" label="First Name" variant="filled" />
+            <TextField onChange={(e) => setFirst(e.target.value)} id="First_Name" label="First Name" variant="filled" />
             <TextField onChange={(e) => setLast(e.target.value)} id="Last_Name" label="Last Name" variant="filled" /> 
-            <TextField onChange={(e) => setEmail(e.target.value)}id="email" label="email" variant="filled" />  */}
+            <TextField onChange={(e) => setEmail(e.target.value)}id="email" label="email" variant="filled" /> 
           </Box>
           </Link>
         </span>}
